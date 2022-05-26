89aa57183e56fb5300f082e8d781c975fb5c2638
Kingkoara <93708295+Kingkouara@users.noreply.github.com>
2022-05-26 20:14:47 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ColorfulMessage from "./components/colorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+  // const contentStyle = {
+  //   color: "blue",
+  //   fontSize: "18px"
+  // };
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue" message="お元気ですか" />
+      <ColorfulMessage color="pink" message="あひあひ" />
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/colorfulMessage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ColorfulMessage = (props) => {
+  //分割代入
+  const { color, message } = props;
+
+  const contentStyle = {
+    color: color,
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>{message}</p>;
+};
+
+export default ColorfulMessage;
